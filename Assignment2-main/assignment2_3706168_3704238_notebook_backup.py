05e26edff170130474168ed36e0bd15bbab53d0d
Niels <niels@looije.net>
2024-05-14 22:14:40 +0200

    level 1.0

--- Assignment2-main/assignment2_3706168_3704238_notebook_backup.py
+++ Assignment2-main/assignment2_3706168_3704238_notebook_backup.py
@@ -21,7 +21,7 @@ class FloodFillSolver():
         :type history: dict[tuple[int], tuple[int]]
     """
     
-    def _call_(self, road_grid, source, destination):
+    def __call__(self, road_grid, source, destination):
         """
         This method gives a shortest route through the grid from source to destination.
         You start at the source and the algorithm ends if you reach the destination, both coordinates should be included in the path.
@@ -127,7 +127,144 @@ class FloodFillSolver():
             new_node = (node[0] + dx, node[1] + dy)
             if 0<=new_node[0]<self.road_grid.shape[0] and 0<= new_node[1] < self.road_grid.shape[1]:
                 steps.append(new_node)
-        return steps
+        return steps
+
+############ CODE BLOCK 10 ################
+
+class GraphBluePrint():
+    """
+    You can ignore this class, it is just needed due to technicalities.
+    """
+    def find_nodes(self): pass
+    def find_edges(self): pass
+    
+class Graph(GraphBluePrint):   
+    """
+    Attributes:
+        :param adjacency_list: The adjacency list with the road distances and speed limit.
+        :type adjacency_list: dict[tuple[int]: set[edge]], where an edge is a fictional datatype 
+                              which is a tuple containing the datatypes tuple[int], int, float
+        :param map: The map of the graph.
+        :type map: Map
+    """
+    def __init__(self, map_, start=(0, 0)):
+        """
+        This function transforms any (city or lower) map into a graph representation.
+
+        :param map_: The map that needs to be transformed.
+        :type map_: Map
+        :param start: The start node from which we will find all other nodes.
+        :type start: tuple[int]
+        """
+        self.adjacency_list = {}
+        self.map = map_
+        self.start = start
+        
+        self.find_nodes()
+        self.find_edges()  # This will be implemented in the next notebook cell
+        
+    def find_nodes(self):
+        """
+        This method contains a breadth-frist search algorithm to find all the nodes in the graph.
+        So far, we called this method `step`. However, this class is more than just the search algorithm,
+        therefore, we gave it a bit more descriptive name.
+
+        Note, that we only want to find the nodes, so history does not need to contain a partial path (previous node).
+        In `find_edges` (the next cell), we will add edges for each node.
+        """
+        queue = deque([self.start])
+        history = {self.start}
+        raise NotImplementedError("Please complete this method")
+                    
+    def adjacency_list_add_node(self, coordinate, actions):
+        """
+        This is a helper function for the breadth-first search algorithm to add a coordinate to the `adjacency_list` and
+        to determine if a coordinate needs to be added to the `adjacency_list`.
+
+        Reminder: A coordinate should only be added to the adjacency list if it is a corner, a crossing, or a dead end.
+                  Adding the coordinate to the adjacency_list is equivalent to saying that it is a node in the graph.
+
+        :param coordinate: The coordinate that might need to be added to the adjacency_list.
+        :type coordinate: tuple[int]
+        :param actions: The actions possible from this coordinate, an action is defined as an action in the coordinate state-space.
+        :type actions: list[tuple[int]]
+        """
+        raise NotImplementedError("Please complete this method")
+                           
+    def neighbour_coordinates(self, coordinate):
+        """
+        This method returns the next possible actions and is part of the breadth-first search algorithm.
+        Similar to `find_nodes`, we often call this method `next_step`.
+        
+        :param coordinate: The current coordinate
+        :type coordinate: tuple[int]
+        :return: A list with possible next coordinates that can be visited from the current coordinate.
+        :rtype: list[tuple[int]]  
+        """
+        raise NotImplementedError("Please complete this method")
+    
+    def __repr__(self):
+        """
+        This returns a representation of a graph.
+
+        :return: A string representing the graph object.
+        :rtype: str
+        """
+        # You can change this to anything you like, such that you can easily print a Graph object. An example is already given.
+        return repr(dict(sorted(self.adjacency_list.items()))).replace("},", "},\n")
+
+    def __getitem__(self, key):
+        """
+        A magic method that makes using keys possible.
+        This makes it possible to use self[node] instead of self.adjacency_list[node]
+
+        :return: The nodes that can be reached from the node `key`.
+        :rtype: set[tuple[int]]
+        """
+        return self.adjacency_list[key]
+
+    def __contains__(self, key):
+        """
+        This magic method makes it possible to check if a coordinate is in the graph.
+
+        :return: This returns if the coordinate is in the graph.
+        :rtype: bool
+        """
+        return key in self.adjacency_list
+
+    def get_random_node(self):
+        """
+        This returns a random node from the graph.
+        
+        :return: A random node
+        :rtype: tuple[int]
+        """
+        return tuple(RNG.choice(list(self.adjacency_list)))
+        
+    def show_coordinates(self, size=5, color='k'):
+        """
+        If this method is used before another method that does a plot, it will be plotted on top.
+
+        :param size: The size of the dots, default to 5
+        :type size: int
+        :param color: The Matplotlib color of the dots, defaults to black
+        :type color: string
+        """
+        nodes = self.adjacency_list.keys()
+        plt.plot([n[1] for n in nodes], [n[0] for n in nodes], 'o', color=color, markersize=size)        
+
+    def show_edges(self, width=0.05, color='r'):
+        """
+        If this method is used before another method that does a plot, it will be plotted on top.
+        
+        :param width: The width of the arrows, default to 0.05
+        :type width: float
+        :param color: The Matplotlib color of the arrows, defaults to red
+        :type color: string
+        """
+        for node, edge_list in self.adjacency_list.items():
+            for next_node,_,_ in edge_list:
+                plt.arrow(node[1], node[0], (next_node[1] - node[1])*0.975, (next_node[0] - node[0])*0.975, color=color, length_includes_head=True, width=width, head_width=4*width)
 
 
 ############ END OF CODE BLOCKS, START SCRIPT BELOW! ################
